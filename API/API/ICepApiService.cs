e5c97d345bee8bdab04db04d6bd06db1537405ff
jho28BR <jhonnatan.santos@tradimus.com.br>
2022-05-17 00:10:28 -0300

    ApiConsultaCep v1.0

---
+++ API/API/ICepApiService.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponsive> GetAddressAsync(string cep);
+    }
+}
---
+++ API/API/Program.cs
@@ -0,0 +1,32 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace API
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe o cep: ");
+
+                string cepInformado = Console.ReadLine();
+                Console.WriteLine("Verificando informações do CEP {0}... ", cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.Clear();
+                Console.Write($"\tResultado:\n\nLogradouro: {address.Logradouro},\nBairro: {address.Bairro},\nCidade: {address.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Erro na consulta do cep: " + e.Message);
+            }
+        }
+    }
+}
